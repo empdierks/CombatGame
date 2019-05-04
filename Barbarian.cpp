528d5afb19d2537dd65b2c916ec4de5df3cd4618
empdierks <16980128+empdierks@users.noreply.github.com>
2019-05-04 15:16:20 -0400

    Create Barbarian.cpp

---
+++ Barbarian.cpp
@@ -0,0 +1,29 @@
+// Author: Emily Dierks
+// Date: 07/29/17
+// Description: This program is the abstract base class for the fighters: Vampire, Barbarian,
+// Blue Men, Medusa, and Harry Potter.  This class holds the pure virtual functions that the character
+// classes will override
+
+#ifndef CREATURE_HPP
+#define CREATURE_HPP
+
+#include <string>
+
+class Creature {
+
+protected:
+	int HP;
+	int Armor;
+	std::string type;
+
+public:
+	virtual int attack() = 0;
+	virtual int defend(int d) = 0;
+	void setHP(int);
+	int getHP();
+	void setArmor(int);
+	int getArmor();
+	std::string getType();
+
+};
+#endif // !CREATURE_HPP
